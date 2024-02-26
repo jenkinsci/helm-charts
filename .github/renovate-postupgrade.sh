a51cd8d8070596039c478c54e671a686f55f53c2
Koen van Zuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2024-02-26 23:43:15 +0100

    chore: Fixed Renovate changelog entry (#1029)

--- .github/renovate-postupgrade.sh
+++ .github/renovate-postupgrade.sh
@@ -2,7 +2,8 @@
 
 CHARTVERSION="$(jx-release-version -previous-version=from-file:charts/jenkins/Chart.yaml)"
 export CHARTVERSION
-export DEPNAME="$1"
+depName=$(echo "$1" | tr ' ' '\n' | sort | uniq)
+export DEPNAME="$depName"
 export NEWVERSION="$2"
 
 helm unittest --strict -f 'unittests/*.yaml' charts/jenkins -u
