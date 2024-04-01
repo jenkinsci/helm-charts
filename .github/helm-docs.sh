2ffe6f635f2fe903727deb3d2db76f1766366928
Alexander Brandes <mc.cache@web.de>
2024-04-01 23:51:57 +0200

    Check if 'helm-docs' is available
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -2,6 +2,13 @@
 
 set -euxo pipefail
 
+# check if helm-docs is installed
+if ! command -v helm-docs &> /dev/null
+then
+    echo "helm-docs could not be found. Did you install it?"
+    exit
+fi
+
 # validate docs
 cd charts/jenkins
 helm-docs --template-files=VALUES.md.gotmpl --output-file=VALUES.md
