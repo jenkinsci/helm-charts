be5a70326957eb2b3ea8c787bc9794809c855774
Alexander Brandes <brandes.alexander@web.de>
2024-04-02 06:35:05 +0200

    Update helm-docs.sh
    Co-authored-by: Koen van Zuijlen <8818390+kvanzuijlen@users.noreply.github.com>

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # check if helm-docs is installed
 if ! command -v helm-docs &> /dev/null; then
-    echo "helm-docs could not be found. Did you install it?"
+    echo "helm-docs could not be found. You can install it from https://github.com/norwoodj/helm-docs."
     exit
 fi
 
