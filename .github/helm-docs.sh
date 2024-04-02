01e87930be3d11e9e96f1b38920221f2a3b75281
Alexander Brandes <mc.cache@web.de>
2024-04-02 16:05:38 +0200

    Fix linting
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 # check if helm-docs is installed
-if ! command -v helm-docs &> /dev/null; then
+if ! command -v helm-docs &>/dev/null; then
   echo "helm-docs could not be found. You can install it from https://github.com/norwoodj/helm-docs."
   exit
 fi
